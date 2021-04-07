7dc40e16b4e951555e37f107b98f4969bb35d3e7
Yosef Gamble <yegamble@gmail.com>
2021-04-08 03:12:31 +1200

    clean returns in seed generator

--- tryte-cipher/seedGenerator.go
+++ tryte-cipher/seedGenerator.go
@@ -27,15 +27,15 @@ func RandomPassphraseGenerator(n int) (stringBytes string, err error) {
 // It will return an error if the system's secure random
 // number generator fails to function correctly, in which
 // case the caller should not continue.
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
+func GenerateRandomBytes(n int) (b []byte, err error) {
+	b = make([]byte, n)
+	_, err = rand.Read(b)
 	// Note that err == nil only if we read len(b) bytes.
 	if err != nil {
 		return nil, err
 	}
 
-	return b, nil
+	return
 }
 
 // GenerateRandomSeed returns a securely generated string.
